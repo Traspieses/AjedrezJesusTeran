a81ab96acf2f19474219ff37df3dde7629916e30
Traspieses <traspieses1@gmail.com>
2025-12-10 12:56:11 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // Esto asegura que las rutas sean relativas (./) en lugar de absolutas (/)
-})
